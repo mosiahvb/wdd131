53250a9eb125b9a5d30e918d5f29bbfd998f99f8
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2024-06-16 15:25:58 -0600

    add ons

--- register/register.js
+++ register/register.js
@@ -1,14 +1,57 @@
 let participantCount = 1;
+const button1 = document.getElementById('add');
+const initialParticipant = document.querySelector('.participant1');
 
-function participantTemplate(count) {
-    return `
-        <section class="participant${count}">
-        </section>
-    `;
+
+function addParticipant() {
+    participantCount++;
+    console.log(participantCount);
+
+    participantTemplate(participantCount);
 }
 
-document.getElementById('add').addEventListener('click', function() {
-    participantCount++;
-    const newParticipantHTML = participantTemplate(participantCount);
-    document.querySelector('.participants').insertAdjacentHTML('beforeend', newParticipantHTML);
-});
+function participantTemplate(count) {
+    const newSectionHTML = `
+        <section class="participant${count}">
+            <p>Participant ${count}</p>
+            <div class="item">
+                <label for="fname${count}">First Name<span>*</span></label>
+                <input id="fname${count}" type="text" name="fname" value="" required />
+            </div>
+            <div class="item activities">
+                <label for="activity${count}">Activity #<span>*</span></label>
+                <input id="activity${count}" type="text" name="activity" />
+            </div>
+            <div class="item">
+                <label for="fee${count}">Fee ($)<span>*</span></label>
+                <input id="fee${count}" type="number" name="fee" />
+            </div>
+            <div class="item">
+                <label for="date${count}">Desired Date <span>*</span></label>
+                <input id="date${count}" type="date" name="date" />
+            </div>
+            <div class="item">
+                <p>Grade</p>
+                <select>
+                    <option selected value="" disabled selected></option>
+                    <option value="1">1st</option>
+                    <option value="2">2nd</option>
+                    <option value="3">3rd</option>
+                    <option value="4">4th</option>
+                    <option value="5">5th</option>
+                    <option value="6">6th</option>
+                    <option value="7">7th</option>
+                    <option value="8">8th</option>
+                    <option value="9">9th</option>
+                    <option value="10">10th</option>
+                    <option value="11">11th</option>
+                    <option value="12">12th</option>
+                </select>
+            </div>
+        </section>
+    `;
+    // this makes sure it appears after the initial participant!!
+    initialParticipant.insertAdjacentHTML('afterend', newSectionHTML);
+}
+
+button1.addEventListener('click', addParticipant);
