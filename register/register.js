e54381d964e9b3335202d0f79064f8d883c6e60f
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2024-06-18 18:02:28 -0600

    finish up

--- register/register.js
+++ register/register.js
@@ -1,57 +1,50 @@
+import { successTemplate, participantTemplate } from './Templates.js';
+
 let participantCount = 1;
 const button1 = document.getElementById('add');
-const initialParticipant = document.querySelector('.participant1');
-
+const button2 = document.getElementById('submitButton');
 
 function addParticipant() {
     participantCount++;
-    console.log(participantCount);
-
-    participantTemplate(participantCount);
+    const newParticipantHTML = participantTemplate(participantCount);
+    insertParticipantHTML(newParticipantHTML);
 }
 
-function participantTemplate(count) {
-    const newSectionHTML = `
-        <section class="participant${count}">
-            <p>Participant ${count}</p>
-            <div class="item">
-                <label for="fname${count}">First Name<span>*</span></label>
-                <input id="fname${count}" type="text" name="fname" value="" required />
-            </div>
-            <div class="item activities">
-                <label for="activity${count}">Activity #<span>*</span></label>
-                <input id="activity${count}" type="text" name="activity" />
-            </div>
-            <div class="item">
-                <label for="fee${count}">Fee ($)<span>*</span></label>
-                <input id="fee${count}" type="number" name="fee" />
-            </div>
-            <div class="item">
-                <label for="date${count}">Desired Date <span>*</span></label>
-                <input id="date${count}" type="date" name="date" />
-            </div>
-            <div class="item">
-                <p>Grade</p>
-                <select>
-                    <option selected value="" disabled selected></option>
-                    <option value="1">1st</option>
-                    <option value="2">2nd</option>
-                    <option value="3">3rd</option>
-                    <option value="4">4th</option>
-                    <option value="5">5th</option>
-                    <option value="6">6th</option>
-                    <option value="7">7th</option>
-                    <option value="8">8th</option>
-                    <option value="9">9th</option>
-                    <option value="10">10th</option>
-                    <option value="11">11th</option>
-                    <option value="12">12th</option>
-                </select>
-            </div>
-        </section>
-    `;
-    // this makes sure it appears after the initial participant!!
-    initialParticipant.insertAdjacentHTML('afterend', newSectionHTML);
+function submitForm(event) {
+    event.preventDefault();
+    
+    const totalFee = totalFees();
+    const adultName = document.getElementById('adult_name').value;
+    const info = {
+        adult_name: adultName,
+        number_of_participants: participantCount,
+        fee_total: totalFee.toFixed(2)
+    };
+    const message = successTemplate(info);
+    const summarySection = document.getElementById('summary');
+    summarySection.innerHTML = `<p>${message}</p>`;
+}
+
+function totalFees() {
+    // the selector below lets us grab any element that has an id that begins with "fee"
+    let feeElements = document.querySelectorAll("[id^=fee]");
+    console.log(feeElements);
+    // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
+    // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
+    // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
+    feeElements = [...feeElements];
+    // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
+    // Remember that the text that was entered into the input element will be found in the .value of the element.
+    const total = feeElements.reduce((accumulator, currentElement) => {
+        return accumulator + parseFloat(currentElement.value) || 0;
+    }, 0);
+    return total;
+}
+
+function insertParticipantHTML(html) {
+    const initialParticipant = document.querySelector('.participant1');
+    initialParticipant.insertAdjacentHTML('afterend', html);
 }
 
 button1.addEventListener('click', addParticipant);
+button2.addEventListener('click', submitForm);
