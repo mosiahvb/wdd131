25fce597623d9a438d2cce3517dfc7e6d6737e1a
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2024-07-16 13:52:28 -0600

    double checking

--- register/register.js
+++ register/register.js
@@ -26,15 +26,9 @@ function submitForm(event) {
 }
 
 function totalFees() {
-    // the selector below lets us grab any element that has an id that begins with "fee"
     let feeElements = document.querySelectorAll("[id^=fee]");
     console.log(feeElements);
-    // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
-    // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
-    // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
     feeElements = [...feeElements];
-    // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
-    // Remember that the text that was entered into the input element will be found in the .value of the element.
     const total = feeElements.reduce((accumulator, currentElement) => {
         return accumulator + parseFloat(currentElement.value) || 0;
     }, 0);
